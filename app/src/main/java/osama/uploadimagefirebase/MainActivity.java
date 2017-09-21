50d64df0c2c3682df73cff9ef88be5dd8834a040
osamamohsen <osamamohsen1994@gmail.com>
2017-09-21 17:50:50 +0200

    Commit UploadImageFirebase

---
+++ app/src/main/java/osama/uploadimagefirebase/MainActivity.java
@@ -0,0 +1,127 @@
+package osama.uploadimagefirebase;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnChoose,btnUpload;
+    private ImageView imageView;
+    private ImageView imgFirebase;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    //Firebase
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnChoose = (Button) findViewById(R.id.btnChoose);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+        imageView = (ImageView) findViewById(R.id.imgView);
+        imgFirebase = (ImageView) findViewById(R.id.imgFirebase);
+
+
+        //Firebase init
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void uploadImage() {
+        if(filePath != null){
+            final ProgressDialog progress = new ProgressDialog(this);
+            progress.setTitle("Uploading....");
+            progress.show();
+
+            StorageReference ref= storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progress.dismiss();
+                    Toast.makeText(MainActivity.this, "Uploaded successfully", Toast.LENGTH_SHORT).show();
+                    String imageUrl = taskSnapshot.getDownloadUrl().toString();
+                    Picasso.with(getBaseContext()).load(imageUrl).into(imgFirebase);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progress.dismiss();
+                    Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progres_time = (100*0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    progress.setMessage("Uploaded "+(int)progres_time+" %");
+                }
+            });
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK &&
+                data != null && data.getData() != null)
+        {
+            filePath = data.getData();
+            try
+            {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
